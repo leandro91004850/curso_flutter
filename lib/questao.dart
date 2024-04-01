3401212d44859ee24608df2ebeef866c3bdc77f4
leandroleekrap <leandroleekrap@gmail.com>
2024-04-01 13:51:33 -0300

    Trablhando do margin e width em widget

--- lib/questao.dart
+++ lib/questao.dart
@@ -7,10 +7,15 @@ class Questao extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      texto, 
-      style: const TextStyle(fontSize: 28),
-      textAlign: TextAlign.center,
-      );
+    return Container(
+      width: double.infinity,
+      //margin: const EdgeInsets.all(20),
+      margin: const EdgeInsets.only(top: 40, bottom: 10, left: 20, right: 20),
+      child: Text(
+        texto, 
+        style: const TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+        ),
+    );
   }
 }
